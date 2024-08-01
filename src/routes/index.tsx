2de12e2993f023d81028e4f8e5e06788b06182ed
jeetKasundra19 <kasundra529jeet@gmail.com>
2024-08-02 00:12:06 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
     <>
       <h1>Hi 👋</h1>
       <div>
-        Can't wait to see what you build with qwik!
+        i am jeet
         <br />
         Happy coding.
       </div>
