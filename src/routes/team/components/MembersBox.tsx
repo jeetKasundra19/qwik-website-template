632f0291c2672e9b293ae2d7f408cbe13f7cca1f
jeetKasundra19 <kasundra529jeet@gmail.com>
2024-08-10 11:06:49 +0530

    Crete member section

--- src/routes/team/components/MembersBox.tsx
+++ src/routes/team/components/MembersBox.tsx
@@ -1,11 +1,27 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
 import styles from "./members-box.css?inline";
+import { Image } from "@unpic/qwik";
 
-export default component$(() => {
-  useStyles$(styles);
-  return (
-    <div class="w-full">
-      <div class="member--image-wrapper"></div>
-    </div>
-  );
-});
+interface MemberBoxProps {
+  image: string;
+  name: string;
+  position: string;
+  appearance: string;
+}
+
+export default component$<MemberBoxProps>(
+  ({ image, name, position, appearance }) => {
+    useStyles$(styles);
+    return (
+      <div class="w-full">
+        <div class="member--image-wrapper">
+          <Image src={image} alt={appearance} />
+        </div>
+        <div class="member-details">
+          <h5>{name}</h5>
+          <p>{position}</p>
+        </div>
+      </div>
+    );
+  }
+);
--- src/routes/team/index.tsx
+++ src/routes/team/index.tsx
@@ -1,12 +1,46 @@
 import { component$ } from "@builder.io/qwik";
 import { Hero, DemoBox } from "~/components";
 import MembersBox from "./components/MembersBox";
+import Image1 from "/images/team/one.png";
+import Image2 from "/images/team/two.png";
+import Image3 from "/images/team/three.png";
+import Image4 from "/images/team/four.png";
 
 const heroContent = {
   title: "Meet Our Team of Experts",
   description:
     "Our team at Nutritionist is composed of highly skilled professionals who are passionate about helping you achieve your health and wellness goals. With a diverse range of expertise in nutrition, coaching, and support, our team is dedicated to providing you with the guidance and personalized care you need. Get to know the experts behind our success and discover how they can make a positive impact on your journey to better health.",
 };
+
+const teamList = [
+  {
+    name: "Sarah Mitchell",
+    image: Image1,
+    position: "Founder and CEO",
+    appearance: "Woman with long blonde hair wearing a white shirt",
+  },
+  {
+    name: "Emily Thompson",
+    position: "Chief Operating Officer",
+    image: Image2,
+    appearance:
+      "Woman with long brown hair and glasses, wearing a plaid blazer and holding a notebook",
+  },
+  {
+    name: "John Davis",
+    position: "Chief Financial Officer",
+    image: Image3,
+    appearance:
+      "Man with short light-colored hair and glasses, wearing a grey suit with a patterned tie",
+  },
+  {
+    name: "Rachel Adams",
+    position: "Chief Marketing Officer",
+    image: Image4,
+    appearance:
+      "Woman with shoulder-length brown hair, wearing a plaid blazer and holding a green folder",
+  },
+];
 
 export default component$(() => {
   return (
@@ -16,7 +50,9 @@ export default component$(() => {
       </section>
       <section class="container-regular py-12 lg:py-24 2xl:py-36">
         <div class="grid grid-cols-4 gap-7">
-          <MembersBox />
+          {teamList.map((team, i) => (
+            <MembersBox key={i} {...team} />
+          ))}
         </div>
       </section>
       <section class="container-regular py-12 lg:py-24 2xl:py-36">
